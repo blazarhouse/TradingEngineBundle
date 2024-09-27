d8f49f4a48f5b20f0c3a4ec46354b90454710bc4
alexis <alexiscassan@gmail.com>
2024-09-27 10:30:13 +0200

    Readme

--- src/DependencyInjection/BlazarTradingEngineExtension.php
+++ src/DependencyInjection/BlazarTradingEngineExtension.php
@@ -2,7 +2,6 @@
 
 namespace Blazar\TradingEngine\DependencyInjection;
 
-use Blazar\TradingEngine\Model\TradingAccountInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 
@@ -10,9 +9,6 @@ class BlazarTradingEngineExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $container
-            ->registerForAutoconfiguration(TradingAccountInterface::class)
-            ->addTag('blazar.trading_engine.trading_event_handler')
-        ;
+        // TODO: Implement load() method.
     }
 }
